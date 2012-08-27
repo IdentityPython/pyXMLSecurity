95114ec05290e92f3963e70c46b4144827db0b73
Leif Johansson <leifj@sunet.se>
2012-08-27 09:22:46 +0200

    pyXMLSec initial import

---
+++ README.rst
@@ -0,0 +1,11 @@
+python XML Security
+===================
+
+This is a python implementation of XML-Security - XML-DSIG only right now. There are no
+dependencies except lxml currently.
+
+This code was inspired by https://github.com/andrewdyates/xmldsig (this implementation is
+a refactor and extension of that implementation) and includes a pure-perl RSA implementation
+https://github.com/andrewdyates/rsa_x509_pem by and with permission from Andrew Yates.
+
+This package is available under the NORDUnet BSD license (cf LICENSE.txt)
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1'
+
+install_requires = [
+    'lxml',
+]
+
+
+setup(name='pyXMLSec',
+    version=version,
+    description="Python XML Security",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='xml xml-dsig security digital signature rsa',
+    author='Leif Johansson',
+    author_email='leifj@sunet.se',
+    url='http://blogs.mnt.se',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    package_data = {
+    },
+    zip_safe=False,
+    install_requires=install_requires,
+    requires=install_requires
+)
