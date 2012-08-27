136cff8d4db2b025d3faaa7b7b5bb418ff6999f3
Leif Johansson <leifj@sunet.se>
2012-08-27 12:08:00 +0200

    - more rubust algorithm uri parsing - support for "#"-style IDs - partial support for <Transform/> elts with child-elements - make all exceptions an XMLSecException - first draft: sign - various cleanups

--- src/xmlsec/__init__.py
+++ src/xmlsec/__init__.py
@@ -17,9 +17,15 @@ DS = ElementMaker(namespace=NS['ds'],nsmap=NS)
 # SHA1 digest with ASN.1 BER SHA1 algorithm designator prefix [RSA-SHA1]
 PREFIX = '\x30\x21\x30\x09\x06\x05\x2B\x0E\x03\x02\x1A\x05\x00\x04\x14'
 
-TRANSFORM_ENVELOPED_SIGNATURE = "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
-TRANSFORM_C14_EXC = "http://www.w3.org/2001/10/xml-exc-c14n"
-TRANSFORM_C14_EXC_WITH_COMMENTS = "http://www.w3.org/2001/10/xml-exc-c14n#WithComments"
+import re, htmlentitydefs
+
+TRANSFORM_ENVELOPED_SIGNATURE = 'http://www.w3.org/2000/09/xmldsig#enveloped-signature'
+TRANSFORM_C14N_EXCLUSIVE_WITH_COMMENTS = 'http://www.w3.org/2001/10/xml-exc-c14n#WithComments'
+TRANSFORM_C14N_EXCLUSIVE = 'http://www.w3.org/2001/10/xml-exc-c14n'
+TRANSFORM_C14N_INCLUSIVE = 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
+
+ALGORITHM_DIGEST_SHA1 = "http://www.w3.org/2000/09/xmldsig#sha1"
+ALGORITHM_SIGNATURE_RSA_SHA1 = "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
 
 # This code was inspired by https://github.com/andrewdyates/xmldsig
 # and includes https://github.com/andrewdyates/rsa_x509_pem with
@@ -90,25 +96,46 @@ def _digest(str,hash_alg):
     digest = b64e(h.digest())
     return digest
 
+def _get_by_id(t,id_v):
+    for id_a in _id_attributes:
+        logging.debug("Looking for #%s using id attribute '%s'" % (id_v,id_a))
+        elts = t.xpath("//*[@%s='%s']" % (id_a,id_v))
+        if elts is not None and len(elts) > 0:
+            return elts[0]
+    return None
+
+def _alg(elt):
+    uri = elt.get('Algorithm',None)
+    if uri is None:
+        return None
+    else:
+        return uri.rstrip('#')
+
 def _process_references(t,sig=None):
     if sig is None:
         sig = t.find(".//{%s}Signature" % NS['ds'])
     for ref in sig.findall(".//{%s}Reference" % NS['ds']):
         object = None
         uri = ref.get('URI',None)
-        if uri is not None or uri == '#' or uri == '':
+        if uri is None or uri == '#' or uri == '':
             ct = copy.deepcopy(t)
             object = ct.getroot()
+        elif uri.startswith('#'):
+            ct = copy.deepcopy(t)
+            object = _get_by_id(ct,uri[1:])
         else:
-            raise ValueError("unknown reference %s" % uri)
+            raise XMLSigException("unknown reference %s" % uri)
+
+        if object is None:
+            raise XMLSigException("Unable to dereference Reference URI='%s'" % uri)
 
         for tr in ref.findall(".//{%s}Transform" % NS['ds']):
-            object = _transform(tr.get('Algorithm',None),object)
+            object = _transform(_alg(tr),object,tr)
 
         dm = ref.find(".//{%s}DigestMethod" % NS['ds'])
         if dm is None:
-            raise ValueError("Unable to find DigestMethod")
-        hash_alg = (dm.get('Algorithm').split("#"))[1]
+            raise XMLSigException("Unable to find DigestMethod")
+        hash_alg = (_alg(dm).split("#"))[1]
         logging.debug("using hash algorithm %s" % hash_alg)
         digest = _digest(object,hash_alg)
         logging.debug("digest for %s: %s" % (uri,digest))
@@ -129,16 +156,9 @@ def _cert(sig,keyspec):
         data = keyspec
 
     if data is None:
-        raise ValueError("Unable to find anything useful to verify with")
+        raise XMLSigException("Unable to find anything useful to verify with")
 
     return data
-
-import re, htmlentitydefs
-
-TRANSFORM_ENVELOPED_SIGNATURE = 'http://www.w3.org/2000/09/xmldsig#enveloped-signature'
-TRANSFORM_C14N_EXCLUSIVE_WITH_COMMENTS = 'http://www.w3.org/2001/10/xml-exc-c14n#WithComments'
-TRANSFORM_C14N_EXCLUSIVE = 'http://www.w3.org/2001/10/xml-exc-c14n'
-TRANSFORM_C14N_INCLUSIVE = 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
 
 ##
 # Removes HTML or XML character references and entities from a text string.
@@ -178,27 +198,41 @@ def _enveloped_signature(t):
     sig.getparent().remove(sig)
     return t
 
-def _c14n(t,exclusive,with_comments):
-    cxml = etree.tostring(t,method="c14n",exclusive=exclusive,with_comments=with_comments)
+def _c14n(t,exclusive,with_comments,inclusive_prefix_list=None):
+    cxml = etree.tostring(t,method="c14n",exclusive=exclusive,with_comments=with_comments,inclusive_ns_prefixes=inclusive_prefix_list)
     u = _unescape(cxml.decode("utf8",errors='replace')).encode("utf8").strip()
     assert u[0] == '<',XMLSigException("C14N buffer doesn't start with '<'")
     assert u[-1] == '>',XMLSigException("C14N buffer doesn't end with '>'")
     return u
 
-def _transform(uri,t,**kwargs):
+def _transform(uri,t,tr=None):
     if uri == TRANSFORM_ENVELOPED_SIGNATURE:
         return _enveloped_signature(t)
 
     if uri == TRANSFORM_C14N_EXCLUSIVE_WITH_COMMENTS:
-        return _c14n(t,exclusive=True,with_comments=True)
+        nslist = []
+        if tr is not None:
+            elt = tr.find(".//{%s}InclusiveNamespaces" % 'http://www.w3.org/2001/10/xml-exc-c14n#')
+            if elt is not None:
+                nslist = elt.get('PrefixList','').split()
+        return _c14n(t,exclusive=True,with_comments=True,inclusive_prefix_list=nslist)
 
     if uri == TRANSFORM_C14N_EXCLUSIVE:
-        return _c14n(t,exclusive=True,with_comments=False)
+        nslist = []
+        if tr is not None:
+            elt = tr.find(".//{%s}InclusiveNamespaces" % 'http://www.w3.org/2001/10/xml-exc-c14n#')
+            if elt is not None:
+                nslist = elt.get('PrefixList','').split()
+        return _c14n(t,exclusive=True,with_comments=False,inclusive_prefix_list=nslist)
 
     if uri == TRANSFORM_C14N_INCLUSIVE:
         return _c14n(t,exclusive=False,with_comments=False)
 
-    raise ValueError("unknown or unimplemented transform %s" % uri)
+    raise XMLSigException("unknown or unimplemented transform %s" % uri)
+
+_id_attributes =['ID','id']
+def setID(ids):
+    _id_attributes = ids
 
 def verify(t,keyspec):
     for sig in t.findall(".//{%s}Signature" % NS['ds']):
@@ -216,8 +250,9 @@ def verify(t,keyspec):
 
         si = sig.find(".//{%s}SignedInfo" % NS['ds'])
         cm = si.find(".//{%s}CanonicalizationMethod" % NS['ds'])
-        assert cm is not None and cm.get('Algorithm',None) is not None,XMLSigException("No CanonicalizationMethod")
-        sic = _transform(cm.get('Algorithm'),si)
+        cm_alg = _alg(cm)
+        assert cm is not None and cm_alg is not None,XMLSigException("No CanonicalizationMethod")
+        sic = _transform(cm_alg,si)
         digest = _digest(sic,"sha1")
         logging.debug("SignedInfo digest: %s" % digest)
         b_digest = b64d(digest)
@@ -229,6 +264,30 @@ def verify(t,keyspec):
         assert expected == actual,XMLSigException("Signature validation failed")
 
     return True
+
+## TODO - support transforms with arguments
+def _signed_info_transforms(transforms):
+    ts = [DS.Transform(Algorithm=t) for t in transforms]
+    return DS.Transforms(*ts)
+
+def _enveloped_signature_template(c14n_method,digest_alg,transforms):
+    return DS.Signature(
+        DS.SignedInfo(
+            DS.CanonicalizationMethod(Algorithm=c14n_method),
+            DS.SignatureMethod(Algorithm=ALGORITHM_SIGNATURE_RSA_SHA1),
+            DS.Reference(
+                _signed_info_transforms(transforms),
+                DS.DigestMethod(Algorithm=digest_alg),
+                DS.DigestValue(),
+                URI=""
+            )
+        )
+    )
+
+def add_enveloped_signature(t,c14n_method=TRANSFORM_C14N_INCLUSIVE,digest_alg=ALGORITHM_DIGEST_SHA1,transforms=None):
+    if transforms is None:
+        transforms = (TRANSFORM_ENVELOPED_SIGNATURE,TRANSFORM_C14N_EXCLUSIVE_WITH_COMMENTS)
+    t.getroot().insert(0,_enveloped_signature_template(c14n_method,digest_alg,transforms))
 
 def sign(t,key_spec,cert_file):
 
@@ -252,8 +311,9 @@ def sign(t,key_spec,cert_file):
 
         si = sig.find(".//{%s}SignedInfo" % NS['ds'])
         cm = si.find(".//{%s}CanonicalizationMethod" % NS['ds'])
-        assert cm is not None and cm.get('Algorithm',None) is not None,XMLSigException("No CanonicalizationMethod")
-        sic = _transform(cm.get('Algorithm'),si)
+        cm_alg = _alg(cm)
+        assert cm is not None and cm_alg is not None,XMLSigException("No CanonicalizationMethod")
+        sic = _transform(cm_alg,si)
         digest = _digest(sic,"sha1")
         logging.debug("SignedInfo digest: %s" % digest)
 
