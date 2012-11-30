559429dab60218938c34997481ebb0f3ad4e3d48
Leif Johansson <leifj@sunet.se>
2012-11-30 10:29:39 +0100

    cut 0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.6dev'
+version = '0.6'
 
 install_requires = [
     'lxml'
