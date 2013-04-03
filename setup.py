e1fffcdaa3ead811cc5b1c8023692f64fec03d15
Leif Johansson <leifj@sunet.se>
2013-04-03 09:07:37 +0200

    cut 0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.8dev'
+version = '0.8'
 
 install_requires = [
     'lxml'
