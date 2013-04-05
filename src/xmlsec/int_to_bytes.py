ffeb68709785234714a8b29f6ad2031d73e982bb
Leif Johansson <leifj@sunet.se>
2013-04-05 10:00:10 +0200

    pep8 cleanup

--- src/xmlsec/int_to_bytes.py
+++ src/xmlsec/int_to_bytes.py
@@ -8,20 +8,21 @@
 """
 __authors__ = ['"Andrew D. Yates" <andrewyates.name@gmail.com>']
 
+
 def bytes_to_int(s):
-  """Return converted bytestring to integer.
+    """Return converted bytestring to integer.
 
   Args:
     s: str of bytes
   Returns:
     int: numeric interpretation of binary string `s`
   """
-  # int type casts may return a long type
-  return int(s.encode('hex'), 16)
+    # int type casts may return a long type
+    return int(s.encode('hex'), 16)
 
 
 def int_to_bytes(num):
-  """Return converted integer to bytestring.
+    """Return converted integer to bytestring.
 
   Note: string encoding is faster than divmod(num, 256) in Python.
 
@@ -32,23 +33,23 @@ def int_to_bytes(num):
   Raises:
     ValueError: `num` is not a non-negative integer
   """
-  if not is_natural(num, include_zero=True):
-    raise ValueError("%s is not a non-negative integer.")
-  hexed = "%x" % num
-  # align hexadecimal string to byte boundaries
-  if len(hexed) % 2 == 1:
-    hexed = '0%s' % hexed
-  return hexed.decode('hex')
+    if not is_natural(num, include_zero=True):
+        raise ValueError("%s is not a non-negative integer.")
+    hexed = "%x" % num
+    # align hexadecimal string to byte boundaries
+    if len(hexed) % 2 == 1:
+        hexed = '0%s' % hexed
+    return hexed.decode('hex')
 
 
 def is_natural(value, include_zero=False):
-  """Return if value is a natural integer in Python.
+    """Return if value is a natural integer in Python.
   
   Returns:
     bool: is value a natural number?
   """
-  return all((
-    isinstance(value, (int, long)),
-    value >= 0,
-    not (value == 0 and not include_zero),
-  ))
+    return all((
+        isinstance(value, (int, long)),
+        value >= 0,
+        not (value == 0 and not include_zero),
+    ))
--- src/xmlsec/test/transform_test.py
+++ src/xmlsec/test/transform_test.py
@@ -7,19 +7,19 @@ import lxml.etree as etree
 import xmlsec
 from xmlsec.test.case import XMLTestData
 
-class TestTransforms(unittest.TestCase):
 
+class TestTransforms(unittest.TestCase):
     def setUp(self):
         self.cases = {}
-        for case_n in pkg_resources.resource_listdir(__name__,"data/transform"):
-            case = XMLTestData(__name__,"data/transform/%s" % case_n)
+        for case_n in pkg_resources.resource_listdir(__name__, "data/transform"):
+            case = XMLTestData(__name__, "data/transform/%s" % case_n)
             self.cases[case_n] = case
 
     def test_enveloped1(self):
         case = self.cases['enveloped1']
         out = xmlsec._transform('http://www.w3.org/2000/09/xmldsig#enveloped-signature',
                                 case.as_etree('in.xml', remove_whitespace=False))
-        self.assertEqual(case.as_buf('out.xml'),etree.tostring(out))
+        self.assertEqual(case.as_buf('out.xml'), etree.tostring(out))
 
     def test_c14n_1(self):
         """
@@ -33,9 +33,10 @@ class TestTransforms(unittest.TestCase):
         """
         Test that whitespaces and newlines are removed properly.
         """
-        data   = '<a> <b> 1 </b> </a>'
+        data = '<a> <b> 1 </b> </a>'
         expect = '<a><b> 1 </b></a>'
         self.assertEqual(_c14n_parse_test(data), expect)
+
 
 def _c14n_parse_test(data):
     xml = xmlsec.parse_xml(data)
@@ -43,8 +44,10 @@ def _c14n_parse_test(data):
     print "C14N output : %s" % (out)
     return out
 
+
 def main():
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
