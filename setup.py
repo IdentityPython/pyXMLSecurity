dab79597debdbc86d200dfe54b1e8ad6bbe794bd
Leif Johansson <leifj@sunet.se>
2013-10-11 17:22:48 +0200

    cut 0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.10.1'
+version = '0.11'
 
 install_requires = [
     'lxml',
