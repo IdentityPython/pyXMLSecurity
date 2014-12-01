b56f901400c99e22dba701d6ccbf4f292c512b3e
Leif Johansson <leifj@sunet.se>
2014-12-01 09:00:48 +0100

    cut 0.14

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.14dev'
+version = '0.14'
 
 install_requires = [
     'lxml', 'pyconfig'
