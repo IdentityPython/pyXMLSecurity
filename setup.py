ad816630beca3a0ec29bb3fd0834ba3e448493e1
Leif Johansson <leifj@sunet.se>
2018-05-25 19:45:13 +0200

    cut 0.18

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.18dev0'
+version = '0.18'
 
 install_requires = [
     'defusedxml', 'lxml', 'pyconfig', 'requests'
