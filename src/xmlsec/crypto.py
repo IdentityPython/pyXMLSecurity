568d81c2755956146e0fb4820b45cbdc3bb9d3c1
Leif Johansson <leifj@sunet.se>
2019-01-20 21:16:35 +0100

    avoid end-of-string newline

--- src/xmlsec/crypto.py
+++ src/xmlsec/crypto.py
@@ -14,7 +14,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding, utils
 from cryptography.x509 import load_pem_x509_certificate, load_der_x509_certificate, Certificate
-import codecs
+import base64
 
 if six.PY2:
     from UserDict import DictMixin
@@ -232,7 +232,7 @@ class XMLSecCryptoREST(XMlSecCrypto):
             msg = r.json()
             if not 'signed' in msg:
                 raise ValueError("Missing signed data in response message")
-            return codecs.encode(msg['signed'], 'base64')
+            return base64.encodestring(msg['signed'])
         except Exception as ex:
             from traceback import print_exc
             print_exc(ex)
