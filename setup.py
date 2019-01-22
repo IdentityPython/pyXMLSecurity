e64f4adefc3a555e89d67accfda7ccdb926d8235
Leif Johansson <leifj@sunet.se>
2019-01-22 14:04:45 +0100

    prep 0.20dev1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.19'
+version = '0.20dev1'
 
 install_requires = [
     'defusedxml', 'lxml', 'pyconfig', 'requests', 'cryptography', 'six'
