fbaea7545c3b9aa3b99380e693b1790b343ce96e
Leif Johansson <leifj@sunet.se>
2021-03-31 10:17:05 +0200

    clarify

--- src/xmlsec/__init__.py
+++ src/xmlsec/__init__.py
@@ -337,7 +337,7 @@ def _verify(t, keyspec, sig_path=".//{%s}Signature" % NS['ds'], drop_signature=F
                 if not this_cert.verify(b64d(sv), actual, sig_digest_alg):
                     raise XMLSigException("Failed to validate {!s} using sig digest {!s} and cm {!s}".format(etree.tostring(sig), sig_digest_alg, cm_alg))
                 validated.append(obj)
-        except (XMLSigException, ValueError) as ex:
+        except (XMLSigException, ValueError) as ex: # we will try the next available signature
             log.error(ex)
 
     if not validated:
